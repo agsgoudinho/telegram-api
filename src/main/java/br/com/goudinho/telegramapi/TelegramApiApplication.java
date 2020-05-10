43a8ba896480b62673ecb47e377320f9982a9533
agsgoudinho <aguinaldogoudinho@gmail.com>
2020-05-10 14:49:35 -0300

    First commit

---
+++ src/main/java/br/com/goudinho/telegramapi/TelegramApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.goudinho.telegramapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/goudinho/telegramapi/TelegramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.goudinho.telegramapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
